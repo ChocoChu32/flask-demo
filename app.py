a9a40993dafa10548e830234f7f193940cffd2b6
julianchu32 <julian.chu.id32@gmail.com>
2025-11-29 01:18:03 +0800

    添加 POST 请求接口

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import re
 
 app = Flask(__name__)
 
@@ -29,3 +30,35 @@ def get_user(user_id):
     if 1 <= user_id <= len(data):
         return jsonify({"code": "0", "data": data[user_id - 1], "msg": "操作成功"})
     return jsonify({"code": "1", "msg": "用户不存在"})
+
+
+@app.route("/register", methods=['POST'])
+def user_register():
+    username = request.json.get("username").strip()  # 用户名
+    password = request.json.get("password").strip()  # 密码
+    sex = request.json.get("sex", "0").strip()  # 性别，默认为0(男性)
+    telephone = request.json.get("telephone", "").strip()  # 手机号，默认为空串
+    address = request.json.get("address", "").strip()  # 地址，默认为空串
+    if username and password and telephone:
+        if username == "wintest":
+            return jsonify({"code": 2002, "msg": "用户名已存在！！！"})
+        elif not (sex == "0" or sex == "1"):
+            return jsonify({"code": 2003, "msg": "输入的性别只能是 0(男) 或 1(女)！！！"})
+        elif not (len(telephone) == 11 and re.match("^1[3,5,7,8]\d{9}$", telephone)):
+            return jsonify({"code": 2004, "msg": "手机号格式不正确！！！"})
+        else:
+            return jsonify({"code": 0, "msg": "恭喜，注册成功！"})
+    else:
+        return jsonify({"code": 2001, "msg": "用户名/密码/手机号不能为空，请检查！！！"})
+
+
+@app.route("/login", methods=['POST'])
+def user_login():
+    username = request.values.get("username")
+    password = request.values.get("password")
+    if username and password:
+        if username == "wintest" and password == "123456":
+            return jsonify({"code": 0, "msg": "恭喜，登录成功！"})
+        return jsonify({"code": 1002, "msg": "用户名或密码错误！！！"})
+    else:
+        return jsonify({"code": 1001, "msg": "用户名或密码不能为空！！！"})
